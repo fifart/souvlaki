459905fe1ed9be3cabf334b2ca463a4f450c1a91
fifart <fifart@gmail.com>
2019-08-27 17:30:54 +0300

    updated

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,14 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(public navCtrl: NavController,private iab: InAppBrowser /* 2 */) {}
+ngOnInit(){
+
+}
+
+openOnline(){
+  const browser = this.iab.create('https://www.thelosouvlakia.gr/adetailframelast.aspx?id=3749','_self',{location:'no'});
+  
+}
 
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { NavController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
+export class TabsPage implements OnInit, OnDestroy, AfterViewInit {
+  public unsubscribeBackEvent: any;
+  backButtonSubscription;
+  constructor(public navCtrl: NavController,private iab: InAppBrowser, private platform: Platform /* 2 */) {}
 
-  constructor() {}
+  ngOnInit(){}
+  ngAfterViewInit() {
+    this.backButtonSubscription = this.platform.backButton.subscribe(() => {
+      navigator['app'].exitApp();
+    });
+  }
+  ngOnDestroy() {
+    this.backButtonSubscription.unsubscribe();
+  }
+
+openOnline(){
+  const browser = this.iab.create('https://www.thelosouvlakia.gr/adetailframelast.aspx?id=3749','_self',{location:'no'});
+  
+}
+
+
 
 }
